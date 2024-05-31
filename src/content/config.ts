8966ec639ae7dd59aefa2d1193db0edee98b8ef9
tonypinkevych <tony@aninix.com>
2024-05-31 19:23:18 +0200

    chore: split by languages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({
+const en = defineCollection({
   type: 'content',
   // Type-check frontmatter using a schema
   schema: z.object({
@@ -13,4 +13,30 @@ const blog = defineCollection({
   }),
 })
 
-export const collections = { blog }
+const ru = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.boolean(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+})
+
+const ua = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.boolean(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+})
+
+export const collections = { en, ru, ua }
