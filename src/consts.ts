0530f6486b96b139d01a71d47e8269d757c551e1
tonypinkevych <tony@aninix.com>
2024-08-26 02:39:56 +0200

    feat: add sitemap

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = "Tony Pinkevych's blog"
 export const SITE_DESCRIPTION = 'Welcome to my personal blog'
+export const SITE_URL = 'https://tonypinkevych.com'
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,43 @@
+import { getCollection } from 'astro:content'
+import { SITE_URL } from '../consts'
+
+function formatDate(date: Date) {
+  return date.toISOString().split('T')[0]
+}
+
+export async function GET() {
+  const posts = await getCollection('en')
+  const ruPosts = await getCollection('ru')
+  const publications = await getCollection('publications')
+
+  const pages = [
+    '',
+    'ru',
+    'publications',
+    ...posts.map((post) => `en/${post.slug}`),
+    ...ruPosts.map((post) => `ru/${post.slug}`),
+    ...publications.map((post) => `publications/${post.slug}`),
+  ]
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map(
+          (page) => `
+        <url>
+          <loc>${SITE_URL}/${page}</loc>
+          <lastmod>${formatDate(new Date())}</lastmod>
+        </url>
+      `
+        )
+        .join('')}
+    </urlset>
+  `.trim()
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+}
